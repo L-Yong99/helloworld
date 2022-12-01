827793bf1555517da2703f8a9af575f1d6722469
He Xiang Koh <kohhxx6@gmail.com>
2022-12-01 21:54:59 +0800

    can delete itinerary now and fixed css

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -40,40 +40,40 @@ class ItinerariesController < ApplicationController
       itinerary.save
       redirect_to plan_itinerary_path(itinerary)
    else
-    copy_itinerary = Itinerary.find(params[:data].to_i)
-    copy_itinerary_address = copy_itinerary.address
-    copy_itinerary_image= copy_itinerary.image
-    title = params[:title]
-    date_range = params[:date]
-    start_date_arr = date_range.split(' ')[0].split('-')
-    end_date_arr = date_range.split(' ')[2].split('-')
-    start_date = Date.new(start_date_arr[0].to_i,start_date_arr[1].to_i,start_date_arr[2].to_i)
-    end_date = Date.new(end_date_arr[0].to_i,end_date_arr[1].to_i,end_date_arr[2].to_i)
-    date_arr = (start_date..end_date).to_a
-    travel_days = (end_date - start_date).to_i + 1
-    itinerary = Itinerary.new(title: title,start_date:start_date,end_date:end_date,address:copy_itinerary_address,travel_days:travel_days, image: copy_itinerary_image,vote:0,rating:0)
-    itinerary.user = current_user
-    itinerary.save
+      copy_itinerary = Itinerary.find(params[:data].to_i)
+      copy_itinerary_address = copy_itinerary.address
+      copy_itinerary_image= copy_itinerary.image
+      title = params[:title]
+      date_range = params[:date]
+      start_date_arr = date_range.split(' ')[0].split('-')
+      end_date_arr = date_range.split(' ')[2].split('-')
+      start_date = Date.new(start_date_arr[0].to_i,start_date_arr[1].to_i,start_date_arr[2].to_i)
+      end_date = Date.new(end_date_arr[0].to_i,end_date_arr[1].to_i,end_date_arr[2].to_i)
+      date_arr = (start_date..end_date).to_a
+      travel_days = (end_date - start_date).to_i + 1
+      itinerary = Itinerary.new(title: title,start_date:start_date,end_date:end_date,address:copy_itinerary_address,travel_days:travel_days, image: copy_itinerary_image,vote:0,rating:0)
+      itinerary.user = current_user
+      itinerary.save
 
-    copy_activities = Activity.where(itinerary: copy_itinerary)
-    copy_activities.each do |copy_activity|
-      copy_activity_event_sequence = copy_activity.event_sequence
-      copy_activity_day = copy_activity.day
-      copy_activity_place = copy_activity.place
-      if copy_activity.place.booking == true
-        booking = "pending"
-      else
-        booking = "none"
+      copy_activities = Activity.where(itinerary: copy_itinerary)
+      copy_activities.each do |copy_activity|
+        copy_activity_event_sequence = copy_activity.event_sequence
+        copy_activity_day = copy_activity.day
+        copy_activity_place = copy_activity.place
+        if copy_activity.place.booking == true
+          booking = "pending"
+        else
+          booking = "none"
+        end
+        Activity.create(event_sequence:copy_activity_event_sequence, day: copy_activity_day, place: copy_activity_place, itinerary: itinerary, date: date_arr[copy_activity_day-1], status:'pending',booking:booking)
       end
-      Activity.create(event_sequence:copy_activity_event_sequence, day: copy_activity_day, place: copy_activity_place, itinerary: itinerary, date: date_arr[copy_activity_day-1], status:'pending',booking:booking)
-    end
-    redirect_to plan_itinerary_path(itinerary)
-    end
+      redirect_to plan_itinerary_path(itinerary)
+      end
 end
 
   def destroy
     @itinerary = Itinerary.find(params[:id])
-    @itinerart.destroy
+    @itinerary.destroy
 
     redirect_to dashboard_itineraries_path, status: :see_other
   end
