c8110fe7dee3c37b3c8c0e067b7797fc7bc3e5f3
He Xiang Koh <kohhxx6@gmail.com>
2022-12-05 09:42:37 +0800

    delete fixed

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,7 +2,7 @@ class Itinerary < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  has_many :lists
+  has_many :lists, dependent: :destroy
   belongs_to :user
   has_many :users_itineraries
   has_many :activities, dependent: :destroy
